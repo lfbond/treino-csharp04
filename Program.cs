73de46d5a0529028f9a2bf5c3c1ce91f9ae2bcd3
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-27 16:32:50 -0300

    Commit  Mod.05-Sub.01-Exercicios da sub.01-incompletos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Exemplo 01 - Mod.05-Sub-01 Und. 01 a 04
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{random.Next(1, 100)}");
+    }
+
+    Console.WriteLine();
+}
